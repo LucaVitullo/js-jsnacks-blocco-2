bd16dce977307573a7aca80f06df3d4baa5d1845
Lucavit9 <95365833+Lucavit9@users.noreply.github.com>
2022-03-11 14:13:26 +0100

    exercise 2 version 1 completed

---
+++ jsnack2/script.js
@@ -0,0 +1,13 @@
+/*Il software deve chiedere per 5 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti. Esegui questo programma in due versioni, con il for e con il while.*/
+
+///VERSIONE CON IL FOR///
+
+/*let somma=0;
+for(let i = 0; i < 5; i++){
+    let numero =parseInt(prompt("inserisci il numero"));
+    somma += numero;
+} 
+console.log(somma);*/
+
+///VERSIONE CON IL WHILE//
+
